f8bd35fad552e431a404ac492cade7cb91c143de
Swarnim Arun <swarnim@deepsource.io>
2023-01-20 15:46:58 +0530

    feat: `dracula` v0.1 rust src

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# line-munch
-Library to calculate lines of code for a given project. Works for all languages.
+# "Count" Dracula
+
+A parser + utils for counting lines and figuring out their meaningfulness for better line counting API.
+
+Can identify lines based on the following criteria:
+- Blank, (contains only whitespace)
+- Meaningless (has no impact on the code, e.g. braces, comments, etc.)
+- Comment (number of lines with comments)
+- String (strings, language strings, raw strings, literal strings, format strings, etc.)
+- Source (the actual code)
+
+## How to run?
+
+Tentative, planned to be made into a library + binary.
+
+The APIs for use as libraries are WIP.
+
+```sh
+# to test
+cargo test
+
+# to run as a dumb util
+cargo run -- <rust|python|cpp|c> /path/to/file
+```
+
+## Table of Contents
+- ["Count" Dracula](#count-dracula)
+  - [How to run?](#how-to-run)
+  - [Table of Contents](#table-of-contents)
+  - [Key Goals](#key-goals)
+  - [Works with](#works-with)
+  - [Dependencies](#dependencies)
+  - [Development](#development)
+  - [Credits](#credits)
+
+## Key Goals
+
+- General enough to work over any language.
+- Performant enough to work with as many files as possible, and allow scanning huge files.
+- Pull Parsing for lazy parsing of the code to allow flexible usage. 
+
+## Works with
+
+- C & C++
+- Python
+- Rust
+- etc...
+
+## Dependencies
+
+Default runtime dependencies: None.
+
+Rust version dependencies:`1.59.0` (Could possibly work with older Rust versions)
+
+## Development
+## Credits
---
+++ src/tests.rs
@@ -0,0 +1,93 @@
+#[cfg(test)]
+mod simple_c {
+
+    #[test]
+    fn try_parse() {
+        use crate::count::*;
+        Parser::new::<C>(
+            r#"
+            // The default entry point for C programs
+            // --------------------------------------
+            // This generally requires C runtime for
+            // the platform to be present.
+            int main(
+                int x,
+                int y,
+                int z,
+            ) {
+                int c = 2;
+                char* str = "hello from C!";
+                /* Multi-Line Comments
+                 seems to work as well */
+                return 0;
+            }
+        "#,
+        )
+        .enumerate()
+        .for_each(|(i, x)| println!("{i}:: {x:?}"));
+    }
+}
+
+#[cfg(test)]
+mod simple_python {
+
+    #[test]
+    fn try_parse() {
+        use crate::count::*;
+        let parsed = Parser::new::<Python>(
+            r#"# some top level comments
+            def main():
+                print("s");"""
+                Multi-line Comments
+                """
+                print(x)
+                """
+                Multi-line Comments
+                """
+            "#,
+        );
+        let mut line_count: usize = 0;
+        let mut stack = vec![];
+        for p in parsed {
+            if matches!(p, ParseOutput::EOL(_) | ParseOutput::EOF) { 
+                if stack.iter().any(|i| matches!(i, ParseOutput::Source(_))) {
+                    line_count += 1;
+                }
+                stack.clear();
+            } else {
+                stack.push(p);
+            }
+        } 
+        assert_eq!(line_count, 3)
+        // .for_each(|(i, x)| println!("{i}:: {x:?}"));
+    }
+}
+
+
+
+#[cfg(test)]
+mod simple_rust {
+
+    #[test]
+    fn try_parse() {
+        use crate::count::*;
+        Parser::new::<Rust>(
+            r##"
+            // The default entry point for C programs
+            // --------------------------------------
+            // This generally requires C runtime for
+            // the platform to be present.
+            fn main() {
+                let c = 2;
+                /* Multi-Line Comments
+                 seems to work as well */
+                 let src = "hello, World!";
+                 let src2 = r#"hello, World!"#;
+                 return 0;
+            }
+        "##,
+        )
+        .enumerate()
+        .for_each(|(i, x)| println!("{i}:: {x:?}"));
+    }
+}
